3d9baa749fb5a1743a5413a3978832fdb1b37483
Michael Adcock <adcockm@gmail.com>
2024-04-02 01:07:06 -0700

    Remove (unused) older compiler tools

--- install-toolchain.sh
+++ install-toolchain.sh
@@ -6,6 +6,4 @@
 #      https://drive.usercontent.google.com/download?id=1MpsE__0Ra_BW3GGMfxvULeX5XeLQDSZR&authuser=0
 #
 wget https://cloud.maschath.de/s/dDrZN4DSgJ33E3w/download/Codescape.GNU.Tools.Package.2019.09-03-2.for.MIPS32.MTI.Bare.Metal.Ubuntu-18.04.5.x86_64.tar.gz
-wget https://cloud.maschath.de/s/aLtJ86KbXmTm6iE/download/Codescape.GNU.Tools.Package.2019.09-03.for.MIPS32.MTI.Bare.Metal.Ubuntu-18.04.5.x86_64.tar.gz
-tar -xzf Codescape.GNU.Tools.Package.2019.09-03.for.MIPS32.MTI.Bare.Metal.Ubuntu-18.04.5.x86_64.tar.gz -C /opt
 tar -xzf Codescape.GNU.Tools.Package.2019.09-03-2.for.MIPS32.MTI.Bare.Metal.Ubuntu-18.04.5.x86_64.tar.gz -C /opt
